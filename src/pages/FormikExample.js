6d7651797b94832c34a4f98f26fac9e1c58894af
Sterba Patrik <sterba@cngroup.dk>
2020-04-09 16:17:51 +0200

    All fields added

--- src/pages/FormikExample.js
+++ src/pages/FormikExample.js
@@ -2,9 +2,12 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { Formik, Field, useField } from 'formik';
-import { Input, Checkbox } from 'antd';
+import { Input, Checkbox, Radio, Select } from 'antd';
 
 import Container from '../components/Container';
+import SecondaryHeading from '../components/SecondaryHeading';
+
+const { Option } = Select;
 
 const TextField = ({ label, ...props }) => {
   const [field] = useField(props);
@@ -19,6 +22,30 @@ const TextField = ({ label, ...props }) => {
   );
 };
 
+const SelectField = ({ label, placeholder, options, ...props }) => {
+  const [field, meta, helper] = useField(props);
+
+  return (
+    <>
+      <div>{label}</div>
+      <Select
+        onChange={(value, option) => {
+          helper.setValue(value);
+          field.onChange(value, option);
+        }}
+        onBlur={(value) => {
+          helper.setTouched(value);
+          field.onBlur(value);
+        }}
+        value={!field.value ? undefined : field.value}
+        placeholder={placeholder}
+      >
+        {options}
+      </Select>
+    </>
+  );
+};
+
 const FormikExample = () => {
   return (
     <Container>
@@ -28,6 +55,9 @@ const FormikExample = () => {
           address: '',
           phoneNumber: '',
           isQuarantined: false,
+          extras: [],
+          paymentMethod: '',
+          customExtra: '',
         }}
       >
         {({ values, handleChange, handleBlur, errors, handleSubmit }) => (
@@ -38,6 +68,55 @@ const FormikExample = () => {
             <Field name='isQuarantined' type='checkbox' as={Checkbox}>
               I am in quarantine
             </Field>
+            <SecondaryHeading>Extras</SecondaryHeading>
+            <Field name='extras' type='checkbox' as={Checkbox} value='cheddar'>
+              Cheddar
+            </Field>
+            <Field name='extras' type='checkbox' as={Checkbox} value='eggs'>
+              Eggs
+            </Field>
+            <Field name='extras' type='checkbox' as={Checkbox} value='chilli'>
+              Chilli
+            </Field>
+            <Field name='extras' type='checkbox' as={Checkbox} value='olives'>
+              Olives
+            </Field>
+            <SecondaryHeading>Method of payment</SecondaryHeading>
+            <Field name='paymentMethod' type='radio' as={Radio} value='card'>
+              Card
+            </Field>
+            <Field name='paymentMethod' type='radio' as={Radio} value='cash'>
+              Cash
+            </Field>
+            <Field
+              name='paymentMethod'
+              type='radio'
+              as={Radio}
+              value='mealTickets'
+            >
+              Meal tickets
+            </Field>
+            <SelectField
+              placeholder='Select an extra'
+              name='customExtra'
+              type='select'
+              label='Chose an Extra'
+              options={[
+                <Option key='fish' value='fish'>
+                  Fish
+                </Option>,
+                <Option key='meat' value='meat'>
+                  Meat
+                </Option>,
+                <Option key='beyondMeat' value='beyondMeat'>
+                  Beyond Meat
+                </Option>,
+                <Option key='vegan' value='vegan'>
+                  Vegan
+                </Option>,
+              ]}
+            />
+
             <SyntaxHighlighter language='json' style={dracula}>
               {JSON.stringify(values, null, 2)}
             </SyntaxHighlighter>
